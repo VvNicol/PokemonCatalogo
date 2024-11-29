571d8a52dc3465db00d4ba61f600c6d850ae567f
nrojlla <csi23-nrojlla@colegioaltair.net>
2024-11-29 09:14:47 +0100

    Organizacion de carpetas y conecxion con la pokeAPI

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  private baseUrl: string = 'https://pokeapi.co/api/v2'
+  constructor(private http: HttpClient) { }
+
+  //Por defecto mostrar 20 pokemons 
+  getPokemonList(limit: number = 20, offset: number = 0): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/pokemon?limit=${limit}&offset=${offset}`);
+  }
+
+  getPokemonDetails(nameOrId: string): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/pokemon/${nameOrId}`);
+  }
+
 }
