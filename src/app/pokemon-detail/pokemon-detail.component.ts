c6051eee77ea7d4b885f9187360475d7ae57bda0
vvale <rojasvillavalentina01@gmail.com>
2024-12-06 17:34:31 +0100

    Intentando redirigir error con cors

--- src/app/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pokemon-detail/pokemon-detail.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonService } from '../services/pokemon.service';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { NgIf } from '@angular/common';
+import { routes } from '../app.routes';
 
 @Component({
   selector: 'app-pokemon-detail',
   standalone: true,
-  imports: [],
+  imports: [MatProgressSpinner, NgIf,],
   templateUrl: './pokemon-detail.component.html',
-  styleUrl: './pokemon-detail.component.css'
+  styleUrls: ['./pokemon-detail.component.css'],
 })
-export class PokemonDetailComponent {
+export class PokemonDetailComponent implements OnInit {
+  pokemon: any = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private pokemonService: PokemonService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id'); // Obtener el ID de la URL
+      if (id) {
+        this.pokemonService.getPokemonDetails(id).subscribe({
+          next: (data) => (this.pokemon = data),
+          error: (error) =>
+            console.error('Error cargando los detalles del Pokémon', error),
+        });
+      }
+    });
+  }
 }
