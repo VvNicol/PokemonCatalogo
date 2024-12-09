2bc3b695c47d2ab57be415a8f3b7ea07d68ad453
vvale <rojasvillavalentina01@gmail.com>
2024-12-09 23:14:52 +0100

    Limpieza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PokemonListComponent } from "./pokemon-list/pokemon-list.component";
-import { PokemonDetailComponent } from "./pokemon-detail/pokemon-detail.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PokemonListComponent, PokemonDetailComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
